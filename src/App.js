937db5f019e7c4f489bbeb31c0cdeddfbeb97bf3
irinaravenova <hernandezirina23@gmail.com>
2023-01-25 13:21:05 -0500

    last working version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddTutorial from "./components/add-tutorial.component";
+import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorials-list.component";
+import Settings from "./components/settings.component";
+import About from "./components/about.component";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark">
+          <Link to={"/add"} className="navbar-brand">
+            Today.
+          </Link>
+          <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Past Entries
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/about"} className="nav-link">
+                About Today
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/settings"} className="nav-link">
+                Settings
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<TutorialsList/>} />
+            <Route path="/tutorials" element={<TutorialsList/>} />
+            <Route path="/add" element={<AddTutorial/>} />
+            <Route path="/tutorials/:id" element={<Tutorial/>} />
+            <Route path="/settings" element={<Settings/>} />
+            <Route path="/about" element={<About/>} />
+          </Routes>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
